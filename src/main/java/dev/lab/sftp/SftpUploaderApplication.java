f6f29f5f07534d43d87d11f9e021a2624a1664ef
chokobar89 <chokobar89@naver.com>
2025-08-30 10:17:22 +0900

    Initial commit

---
+++ src/main/java/dev/lab/sftp/SftpUploaderApplication.java
@@ -0,0 +1,13 @@
+package dev.lab.sftp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SftpUploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SftpUploaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/lab/sftp/SftpUploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lab.sftp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SftpUploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
