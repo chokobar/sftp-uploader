47a4da8c9b58d72e3c476085723cc2211ee65deb
chokobar89 <chokobar89@naver.com>
2025-09-01 19:56:54 +0900

    스케줄러 추카

---
+++ src/main/java/dev/lab/sftp/SftpUploaderScheduler.java
@@ -0,0 +1,22 @@
+package dev.lab.sftp;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class SftpUploaderScheduler {
+
+    private final SftpUploader sftpUploader;
+
+    // 1분마다 실행 (매 분 0초마다)
+    @Scheduled(cron = "0 * * * * *")
+    public void runMoveTask() {
+        log.info("=== A에서 B로 파일이동 시작 ===");
+        sftpUploader.fileMoveExport();
+        log.info("=== A에서 B로 파일이동 종료 ===");
+    }
+}
